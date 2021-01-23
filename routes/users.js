9b3a9c06dc08a5c7399b1a34d5dd1fda026c5dc2
Nicholas Sinai <nicksinai3@gmail.com>
2021-01-23 15:31:28 -0600

    fixed users/cool/ route syntax

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,12 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get("/", function (req, res) {
-  res.send("respond with a resource");
+router.get('/', function (req, res) {
+  res.send('respond with a resource');
 });
 
-router.get("cool", function (req, res) {
+router.get('/cool', function (req, res) {
   res.send("you're so cool!");
 });
 
